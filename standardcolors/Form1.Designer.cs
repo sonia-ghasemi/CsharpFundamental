c49b39fd3a26ce5ae785977c79fce0f4f8dec3bc
sonia ghasemii <soniaghasemi627@outlook.com>
2020-10-31 10:00:33 +0330

    Add project files.

---
+++ standardcolors/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace standardcolors
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.typeLabel = new System.Windows.Forms.Label();
+            this.rCodeLabel = new System.Windows.Forms.Label();
+            this.gCodeLabel = new System.Windows.Forms.Label();
+            this.bCodeLable = new System.Windows.Forms.Label();
+            this.camalabel1 = new System.Windows.Forms.Label();
+            this.camaLabel2 = new System.Windows.Forms.Label();
+            this.titleLabel1 = new System.Windows.Forms.Label();
+            this.countLabel = new System.Windows.Forms.Label();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.previousButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // typeLabel
+            // 
+            this.typeLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.typeLabel.AutoSize = true;
+            this.typeLabel.Font = new System.Drawing.Font("Stencil", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.typeLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.typeLabel.Location = new System.Drawing.Point(363, 84);
+            this.typeLabel.Name = "typeLabel";
+            this.typeLabel.Size = new System.Drawing.Size(169, 32);
+            this.typeLabel.TabIndex = 21;
+            this.typeLabel.Text = "RGB code :";
+            // 
+            // rCodeLabel
+            // 
+            this.rCodeLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.rCodeLabel.AutoSize = true;
+            this.rCodeLabel.Font = new System.Drawing.Font("Stencil", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rCodeLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.rCodeLabel.Location = new System.Drawing.Point(540, 84);
+            this.rCodeLabel.Name = "rCodeLabel";
+            this.rCodeLabel.Size = new System.Drawing.Size(47, 32);
+            this.rCodeLabel.TabIndex = 22;
+            this.rCodeLabel.Text = "    ";
+            this.rCodeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gCodeLabel
+            // 
+            this.gCodeLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.gCodeLabel.AutoSize = true;
+            this.gCodeLabel.Font = new System.Drawing.Font("Stencil", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gCodeLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.gCodeLabel.Location = new System.Drawing.Point(628, 84);
+            this.gCodeLabel.Name = "gCodeLabel";
+            this.gCodeLabel.Size = new System.Drawing.Size(47, 32);
+            this.gCodeLabel.TabIndex = 23;
+            this.gCodeLabel.Text = "    ";
+            this.gCodeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // bCodeLable
+            // 
+            this.bCodeLable.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.bCodeLable.AutoSize = true;
+            this.bCodeLable.Font = new System.Drawing.Font("Stencil", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bCodeLable.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.bCodeLable.Location = new System.Drawing.Point(716, 84);
+            this.bCodeLable.Name = "bCodeLable";
+            this.bCodeLable.Size = new System.Drawing.Size(47, 32);
+            this.bCodeLable.TabIndex = 24;
+            this.bCodeLable.Text = "    ";
+            this.bCodeLable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // camalabel1
+            // 
+            this.camalabel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.camalabel1.AutoSize = true;
+            this.camalabel1.Font = new System.Drawing.Font("Stencil", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.camalabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.camalabel1.Location = new System.Drawing.Point(595, 84);
+            this.camalabel1.Name = "camalabel1";
+            this.camalabel1.Size = new System.Drawing.Size(25, 32);
+            this.camalabel1.TabIndex = 25;
+            this.camalabel1.Text = ",";
+            this.camalabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // camaLabel2
+            // 
+            this.camaLabel2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.camaLabel2.AutoSize = true;
+            this.camaLabel2.Font = new System.Drawing.Font("Stencil", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.camaLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.camaLabel2.Location = new System.Drawing.Point(683, 84);
+            this.camaLabel2.Name = "camaLabel2";
+            this.camaLabel2.Size = new System.Drawing.Size(25, 32);
+            this.camaLabel2.TabIndex = 26;
+            this.camaLabel2.Text = ",";
+            this.camaLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // titleLabel1
+            // 
+            this.titleLabel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.titleLabel1.AutoSize = true;
+            this.titleLabel1.Font = new System.Drawing.Font("Stencil", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.titleLabel1.Location = new System.Drawing.Point(73, 84);
+            this.titleLabel1.Name = "titleLabel1";
+            this.titleLabel1.Size = new System.Drawing.Size(227, 32);
+            this.titleLabel1.TabIndex = 27;
+            this.titleLabel1.Text = "Color number";
+            // 
+            // countLabel
+            // 
+            this.countLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.countLabel.AutoSize = true;
+            this.countLabel.Font = new System.Drawing.Font("Stencil", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.countLabel.Location = new System.Drawing.Point(308, 84);
+            this.countLabel.Name = "countLabel";
+            this.countLabel.Size = new System.Drawing.Size(47, 32);
+            this.countLabel.TabIndex = 28;
+            this.countLabel.Text = "    ";
+            this.countLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Font = new System.Drawing.Font("Modern No. 20", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.infoLabel.Location = new System.Drawing.Point(12, 254);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(368, 36);
+            this.infoLabel.TabIndex = 29;
+            this.infoLabel.Text = "Click to change color ...";
+            // 
+            // exitButton
+            // 
+            this.exitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitButton.Font = new System.Drawing.Font("Modern No. 20", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.exitButton.Location = new System.Drawing.Point(654, 247);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(134, 43);
+            this.exitButton.TabIndex = 30;
+            this.exitButton.Text = "EXIT";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.nextButton.FlatAppearance.BorderSize = 0;
+            this.nextButton.Font = new System.Drawing.Font("Verdana", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.nextButton.Location = new System.Drawing.Point(768, 119);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(73, 86);
+            this.nextButton.TabIndex = 31;
+            this.nextButton.Text = ">";
+            this.nextButton.UseVisualStyleBackColor = false;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // previousButton
+            // 
+            this.previousButton.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.previousButton.FlatAppearance.BorderSize = 0;
+            this.previousButton.Font = new System.Drawing.Font("Verdana", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.previousButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.previousButton.Location = new System.Drawing.Point(12, 119);
+            this.previousButton.Name = "previousButton";
+            this.previousButton.Size = new System.Drawing.Size(73, 86);
+            this.previousButton.TabIndex = 32;
+            this.previousButton.Text = "<";
+            this.previousButton.UseVisualStyleBackColor = false;
+            this.previousButton.Click += new System.EventHandler(this.previousButton_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 322);
+            this.Controls.Add(this.previousButton);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.countLabel);
+            this.Controls.Add(this.titleLabel1);
+            this.Controls.Add(this.camaLabel2);
+            this.Controls.Add(this.camalabel1);
+            this.Controls.Add(this.bCodeLable);
+            this.Controls.Add(this.gCodeLabel);
+            this.Controls.Add(this.rCodeLabel);
+            this.Controls.Add(this.typeLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label typeLabel;
+        private System.Windows.Forms.Label rCodeLabel;
+        private System.Windows.Forms.Label gCodeLabel;
+        private System.Windows.Forms.Label bCodeLable;
+        private System.Windows.Forms.Label camalabel1;
+        private System.Windows.Forms.Label camaLabel2;
+        private System.Windows.Forms.Label titleLabel1;
+        private System.Windows.Forms.Label countLabel;
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button previousButton;
+    }
+}
+
---
+++ standardcolors/Form1.cs
@@ -0,0 +1,148 @@
+﻿
+
+namespace standardcolors
+{
+    /// <summary>
+    /// نمایش 216 رنگ استاندارد در بک کالر فرم و تغییر آن به وسیله دکمه های قبل و بعد
+    /// </summary>
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        // سه اندیس ای،جی و کی را بترتیب بعنوان شمارنده                
+        // طیف آبی،سبز و قرمز کد رنگ تعریف میکنیم
+        // و اندیس ان را بعنوان شمارنده رنگ تعریف می کنیم
+
+        int i, j, k, n = 0;
+
+        private void nextButton_Click(object sender, System.EventArgs e)
+        {
+            //باتوجه به کد رنگ های استاندارد در مبنای دسیمال 
+            //هر اندیس در مبنای ده با فواصل پنجاه و یک تایی افزایش میابد
+            //تا زمانی که کوچکتر از 255 باشند 
+            //در هنگام تغییر طیف شمارنده طیف های قبلی را 0 میکنیم
+            //برای آخرین رنگ(216) این دکمه عمل نمیکند
+
+            if (n == 0)
+            {
+                i = 0;
+                j = 0;
+                k = 0;
+                previousButton.BackColor = System.Drawing.Color.FromArgb(k, j, i);
+                nextButton.BackColor = System.Drawing.Color.FromArgb(k, j, i);
+                BackColor = System.Drawing.Color.FromArgb(k, j, i);
+                n++;
+                countLabel.Text = System.Convert.ToString(n);
+                rCodeLabel.Text = System.Convert.ToString(k);
+                gCodeLabel.Text = System.Convert.ToString(j);
+                bCodeLable.Text = System.Convert.ToString(i);
+            }
+
+            else if (i < 255 && n != 0)
+            {
+                n++;
+                i += 51;
+                previousButton.BackColor = System.Drawing.Color.FromArgb(k, j, i);
+                nextButton.BackColor = System.Drawing.Color.FromArgb(k, j, i);
+                BackColor = System.Drawing.Color.FromArgb(k, j, i);
+                countLabel.Text = System.Convert.ToString(n);
+                rCodeLabel.Text = System.Convert.ToString(k);
+                gCodeLabel.Text = System.Convert.ToString(j);
+                bCodeLable.Text = System.Convert.ToString(i);
+            }
+
+            else if (j < 255 && n != 0)
+            {
+                n++;
+                i = 0;
+                j += 51;
+                previousButton.BackColor = System.Drawing.Color.FromArgb(k, j, i);
+                nextButton.BackColor = System.Drawing.Color.FromArgb(k, j, i);
+                BackColor = System.Drawing.Color.FromArgb(k, j, i);
+                countLabel.Text = System.Convert.ToString(n);
+                rCodeLabel.Text = System.Convert.ToString(k);
+                gCodeLabel.Text = System.Convert.ToString(j);
+                bCodeLable.Text = System.Convert.ToString(i);
+            }
+
+            else if (k < 255 && n != 0)
+            {
+                n++;
+                i = 0;
+                j = 0;
+                k += 51;
+                previousButton.BackColor = System.Drawing.Color.FromArgb(k, j, i);
+                nextButton.BackColor = System.Drawing.Color.FromArgb(k, j, i);
+                BackColor = System.Drawing.Color.FromArgb(k, j, i);
+                countLabel.Text = System.Convert.ToString(n);
+                rCodeLabel.Text = System.Convert.ToString(k);
+                gCodeLabel.Text = System.Convert.ToString(j);
+                bCodeLable.Text = System.Convert.ToString(i);
+            }
+
+            //else
+            //{
+            //    System.Windows.Forms.MessageBox.Show("THE END");
+            //}
+        }
+
+        private void previousButton_Click(object sender, System.EventArgs e)
+        {
+            //باتوجه به کد رنگ های استاندارد در مبنای دسیمال 
+            //هر اندیس در مبنای ده با فواصل پنجاه و یک تایی کاهش میابد
+            //تا زمانی که بزرگتر از 0 باشند 
+            //در هنگام تغییر طیف شمارنده طیف های قبلی را 255 میکنیم
+            // برای اولین رنگ این دکمه عمل نمیکند
+
+            if (i > 0 && n > 1)
+            {
+                n--;
+                i -= 51;
+                previousButton.BackColor = System.Drawing.Color.FromArgb(k, j, i);
+                nextButton.BackColor = System.Drawing.Color.FromArgb(k, j, i);
+                BackColor = System.Drawing.Color.FromArgb(k, j, i);
+                countLabel.Text = System.Convert.ToString(n);
+                rCodeLabel.Text = System.Convert.ToString(k);
+                gCodeLabel.Text = System.Convert.ToString(j);
+                bCodeLable.Text = System.Convert.ToString(i);
+            }
+
+            else if (j > 0 && n > 1)
+            {
+                n--;
+                i = 255;
+                j -= 51;
+                previousButton.BackColor = System.Drawing.Color.FromArgb(k, j, i);
+                nextButton.BackColor = System.Drawing.Color.FromArgb(k, j, i);
+                BackColor = System.Drawing.Color.FromArgb(k, j, i);
+                countLabel.Text = System.Convert.ToString(n);
+                rCodeLabel.Text = System.Convert.ToString(k);
+                gCodeLabel.Text = System.Convert.ToString(j);
+                bCodeLable.Text = System.Convert.ToString(i);
+            }
+
+            else if (k > 0 && n > 1)
+            {
+                n--;
+                i = 255;
+                j = 255;
+                k -= 51;
+                previousButton.BackColor = System.Drawing.Color.FromArgb(k, j, i);
+                nextButton.BackColor = System.Drawing.Color.FromArgb(k, j, i);
+                BackColor = System.Drawing.Color.FromArgb(k, j, i);
+                countLabel.Text = System.Convert.ToString(n);
+                rCodeLabel.Text = System.Convert.ToString(k);
+                gCodeLabel.Text = System.Convert.ToString(j);
+                bCodeLable.Text = System.Convert.ToString(i);
+            }
+        }
+
+        private void exitButton_Click(object sender, System.EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+}
